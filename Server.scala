63d0b8071e007e4efbc083747cf2b83764943c46
TristanCates <tcates97@gmail.com>
2020-12-08 11:40:10 -0600

    Adding introduction mesage

--- Server.scala
+++ Server.scala
@@ -8,6 +8,7 @@ object Server extends App {
         var socket = server.accept()
         var input = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         var line = ""
+        System.out.println("Starting server...")
         while ((line = input.readLine()) != null) {
             System.out.println(line)
         }
